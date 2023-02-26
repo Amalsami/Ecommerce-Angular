7fbae075443df474b1726ad125f840bdbba875ba
amal sami <amalsamii13@gmail.com>
2023-02-26 15:08:25 +0200

    editproduct

--- src/app/Component/carousel/carousel.component.ts
+++ src/app/Component/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input ,AfterContentInit, ContentChildren, ViewChild, QueryList, ElementRef } from '@angular/core';
+import { Component, Input, AfterContentInit, ContentChildren, ViewChild, QueryList, ElementRef } from '@angular/core';
 import { DemoService } from 'src/app/Core/Services/demo.service';
 
 @Component({
@@ -7,33 +7,18 @@ import { DemoService } from 'src/app/Core/Services/demo.service';
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent {
-  products:any;
-  images:any=["https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
-  "https://fakestoreapi.com/img/71-3HjGNDUL._AC_SY879._SX._UX._SY._UY_.jpg",
-  "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-  "https://fakestoreapi.com/img/71YXzeOuslL._AC_UY879_.jpg",
-  "https://fakestoreapi.com/img/71pWzhdJNwL._AC_UL640_QL65_ML3_.jpg"]
+  products: any;
 
-  
-  constructor(myService:DemoService){
-    // console.log(myService.GetAllUsers())
-    // myService.GetAllUsers().subscribe(
-    //   (data)=>{console.log(data)},
-    //   (err)=>{console.log(err)}
-    //   )
-  
-   
-    console.log(this.images);
-    
+  constructor(myService: DemoService) {
+
     myService.GetAllProduct().subscribe(
-        {
-          next:(data)=>{
-            
-            this.products = data;
-            console.log(this.products)
-          },
-          error:(err)=>{console.log(err)}
-        }
-      )
+      {
+        next: (data) => {
+          this.products = data;
+          console.log(this.products)
+        },
+        error: (err) => { console.log(err) }
+      }
+    )
   }
 }
